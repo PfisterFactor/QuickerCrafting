32df5e84dd6edd6fbf81ed2f4d098b2c33a79818
ej <EJPfister55@gmail.com>
2019-12-30 14:45:37 -0600

    Forgot to commit API files

---
+++ src/api/java/baubles/api/cap/IBaublesItemHandler.java
@@ -0,0 +1,26 @@
+package baubles.api.cap;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.IItemHandlerModifiable;
+
+public interface IBaublesItemHandler extends IItemHandlerModifiable {
+
+    boolean isItemValidForSlot(int slot, ItemStack stack, EntityLivingBase player);
+
+    /**
+     * Used internally to prevent equip/unequip events from triggering when they shouldn't
+     */
+    boolean isEventBlocked();
+
+    void setEventBlock(boolean blockEvents);
+
+    /**
+     * Used internally for syncing. Indicates if the inventory has changed since last sync
+     */
+    boolean isChanged(int slot);
+
+    void setChanged(int slot, boolean changed);
+
+    void setPlayer(EntityLivingBase player);
+}
